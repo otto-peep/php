414ee259c2991438843debc10a24ec4088a60f9b
Pol CONIN <pconin@e2r2p18.42.fr>
2016-10-05 12:54:44 +0200

    ex08

--- d01/ex08/ft_is_sort.php
+++ d01/ex08/ft_is_sort.php
@@ -1,1 +1,16 @@
-#
+<?php
+	function ft_is_sort($arr)
+	{
+		if (is_array($arr))
+		{
+			$tmp = $arr;
+			sort($tmp);
+			foreach($tmp as $key=>$value)
+			{
+				if (strcmp($value, $arr[$key]) != 0)
+					return false;
+			}
+			return true;
+		}
+	}	
+?>
