6b9a77edd67088c9b7d947f0fc4361ac4f7adf3a
Pol CONIN <pconin@e2r2p18.42.fr>
2016-10-07 11:16:25 +0200

    d04 init

--- d03/ex06/members_only.php
+++ d03/ex06/members_only.php
@@ -1,11 +1,14 @@
 <?php
-	if (isset($_SERVER['PHP_AUTH_USER']) && $_SERVER['PHP_AUTH_USER'] == 'zaz' && isset($_SERVER['PHP_AUT_PW']) && $_SERVER['PHP_AUT_PW'] == "jaimelespetitsponeys") 
+	if ((isset($_SERVER['PHP_AUTH_USER']) && ($_SERVER['PHP_AUTH_USER'] == "zaz")) && (isset($_SERVER['PHP_AUT_PW']) && ($_SERVER['PHP_AUT_PW'] == "jaimelespetitsponeys")))
 	{
-		echo "bravo";
+		$s = file_get_contents('../img/42.png');
+		$s1 = base64_encode($s);
+		echo "<html><body>\nBonjour Zaz<br />\n<img src='data:img/png;base64,".$s1."'>\n</body></html>";
 	}
 	else
 	{
 		header('WWW-Authenticate: Basic realm="Espace membres"');
     	header("HTTP/1.0 401 Unauthorized");
+		print("<body><html>Cette zone est accessible uniquement aux membres du site</body></html>\n");
 	}
 ?>
