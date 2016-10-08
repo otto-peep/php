20612715bca738a2dd7a6e32d934f2f482668746
otto-peep <pconin@gmail.com>
2016-10-08 13:31:15 +0200

    d04

--- d04/ex01/create.php
+++ d04/ex01/create.php
@@ -1,72 +1,44 @@
-<?php /*/
-	session_start();
-	if ($_POST[submit] == "OK")
-	{
-		if ($_POST[login] == null || $_POST[passwd] == null)
-		{
-			echo "ERROR pas de login/mdp\n";
-			exit();
-		}
-		if (file_exists("./private") == false)
-		{	mkdir("./private"); }
-		if (file_exists("./private/passwd") == false)
-		{	file_put_contents("./private/passwd", ""); }
-		$ids = file_get_contents("./private/passwd");
-		$tab = unserialize($ids);
-
-		$log = array("$_POST[login]", "$_POST[passwd]");
-		foreach ($tab as $key=>$value)
-		{
-			$tmp = unserialize($value[0])
-		//	echo ($tmp);
-		//	if (log[0] === unserialize($value[0])
-		//	{
-		//		echo "ERROR l'utilisateur existe deja\n";
-		//		exit ();
-		//	}
-		}		
-		$tab[] = serialize($log);
-		file_put_contents("./private/passwd", serialize($tab));
-		$ids = file_get_contents("./private/passwd");
-		$tab = unserialize($ids);
-		$ex = unserialize($tab[0]);
-		print_r($ex);
-	}
-	else
-	{
-		echo "ERROR submite != OK \n";
-	}
-?>
-/*/?>
 <?php
-	session_start();
-	if ($_POST[submit] !== "OK")
+	if ($_POST['submit'] !== "OK")
 	{	echo("Error SUBMIT NOT OK\n");	
-		exit(); }
-	if ($_POST[login] == NULL || $_POST[passwd] == null)
+		return ;}
+	if ($_POST['login'] && $_POST['passwd'])
 	{
-		echo("Error no login/pwd\n");
-		exit();}
-	$logs = array($_POST[login], $_POST[passwd]);
-	if(file_exists("./private") == false)
-	{	mkdir("./private");}
-	if (file_exists("./private/passwd") == false)
-	{
-		file_put_contents("./private/passwd", "");
+		if (!file_exists('private'))
+		{
+			mkdir('private');
+		}
+		if (file_exists('private/passwd'))
+		{
+			$dtbase = file_get_contents('private/passwd');
+			$array = unserialize($dtbase);
+			foreach($array as $tab)
+			{
+				foreach ($tab as $key => $value)
+				{
+					if ($key === 'login') && ($value === $_POST['login'])
+					{
+						echo "ERROR\n";
+						return ;
+					}
+				}
+			}
+			$logs[] = ['login' => $_POST['login']
+						'passwd' => hash('whirlpool', $_POST['passwd'], FALSE)];
+			file_put_contents('private/passwd', serialize($logs));
+		}
+		else
+		{
+			$logs[] = ['login' => $_POST['login']
+						'passwd' => hash('whirlpool', $_POST['passwd'], FALSE)];
+			file_put_contents('private/passwd', serialize($logs));
+		}
+		echo "OK\n";
 	}
 	else
 	{
-		$content = unserialize(file_get_contents("./private/passwd"));
-//	print($content[1]);
-		foreach ($content as $key=>$value)
-		{
-			if((unserialize($value))[0] === $logs[0])
-			{
-				echo "ERROR doublon","<br/>";
-				exit();
-			}
-		}
+		echo "ERROR\n";
+		return ;
 	}
-	$content[] = serialize($logs);
-	file_put_contents("./private/passwd", serialize($tab));
+
 ?>
---
+++ d04/ex02/modif.php
@@ -0,0 +1,12 @@
+<?php
+	if (!$_POST['login'] || !$_POST['newpw'] || $_POST['submit'] !== "OK" || $_POST['oldpw'])
+	{
+		echo "ERROR\n";
+		return ;
+	}
+	else
+	{
+		
+	}
+
+?>
